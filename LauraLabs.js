83ba1a2da0e9f84d5c6c01caa1af85070caf4088
Laura <laurawmbogo@gmail.com>
2024-12-15 02:25:32 -0600

    Javascript file - Holiday HackaThon
    did not use but attempted code

---
+++ LauraLabs.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+//POPUP Greetings
+// const greetingsPopup = document.querySelector(".greetingsPopup");
+// const openPopup = document.querySelector(".openGreetingsPopup");
+// const closePopup = document.querySelector(".closeGreetingsPopup");
+
+// openPopup.addEventListener("click", () => {
+//   greetingsPopup.show();
+// });
+
+// closePopup.addEventListener("click", () => {
+//   greetingsPopup.close();
+// });
+
+
+//POPUP Testimonials
+//const testimonialsPopup = document.querySelector(".openTestimonials");
+//const openDialogue = document.querySelector(".openTestimonialsPopup");
+//const closeDialogue = document.querySelector(".closeTestimonialsPopup");
+
+//openDialogue.addEventListener("click", () => {
+//  testimonialsPopup.show();
+//});
+
+//closeDialogue.addEventListener("click", () => {
+ // testimonialsPopup.close();
+//});
